386122b1e490eaa0abe4fdc7bc83092e7c33bb6c
heintzz <mhasnanr@gmail.com>
2024-07-18 08:25:03 +0700

    feat: add update delete and edit functionality

--- routes/child.route.js
+++ routes/child.route.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const verifyMiddleware = require('../middleware/verifyToken');
 const childController = require('../controllers/child.controller');
 
-router.get('/', verifyMiddleware.verifyToken, childController.getChildrenList);
-router.post('/', verifyMiddleware.verifyToken, childController.addChildData);
+router.get('/', childController.getChildrenList);
+router.get('/:id', childController.getChildById);
+router.put('/:id', childController.updateChildData);
+router.delete('/:id', childController.deleteChildData);
+router.post('/', childController.addChildData);
 
 module.exports = router;
