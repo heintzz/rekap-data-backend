ec811abb2f5c0b47f4356b83e1726cd9736b3f12
heintzz <mhasnanr@gmail.com>
2024-07-18 08:26:37 +0700

    update: recrord routes on edit and get record specifically

--- routes/record.route.js
+++ routes/record.route.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const verifyMiddleware = require('../middleware/verifyToken');
 const recordController = require('../controllers/record.controller');
 
-router.get('/', verifyMiddleware.verifyToken, recordController.getRecords);
-router.get('/grouped', verifyMiddleware.verifyToken, recordController.getGroupedRecordDateList);
-router.get('/:id', verifyMiddleware.verifyToken, recordController.getRecord);
-router.post('/', verifyMiddleware.verifyToken, recordController.createRecord);
+router.get('/', recordController.getRecords);
+router.get('/grouped', recordController.getGroupedRecordDateList);
+router.get('/:id', recordController.getRecord);
+router.put('/:id', recordController.updateRecord);
+router.post('/', recordController.createRecord);
 
 module.exports = router;
