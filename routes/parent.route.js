a80d12e24021597ee919e8cc58c732b22a517f63
heintzz <mhasnanr@gmail.com>
2024-07-18 08:26:08 +0700

    delete: remove auth functionality on parent controller

--- routes/parent.route.js
+++ routes/parent.route.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const verifyMiddleware = require('../middleware/verifyToken');
 const parentController = require('../controllers/parent.controller');
 
-router.get('/', verifyMiddleware.verifyToken, parentController.getParentsList);
-router.post('/', verifyMiddleware.verifyToken, parentController.addParentData);
+router.get('/', parentController.getParentsList);
+router.post('/', parentController.addParentData);
 
 module.exports = router;
